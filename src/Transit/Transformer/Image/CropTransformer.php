0029c25c5bc744fceee1ec941c05963fa1e22ee9
Miles Johnson <mileswjohnson@gmail.com>
2013-02-19 15:03:34 -0800

    Allow crop location to be an array

--- src/Transit/Transformer/Image/CropTransformer.php
+++ src/Transit/Transformer/Image/CropTransformer.php
@@ -66,30 +66,36 @@ class CropTransformer extends AbstractImageTransformer {
 		$src_w = $baseWidth;
 		$src_h = $baseHeight;
 
+		// If an array is passed, use those dimensions
+		if (is_array($location)) {
+			list($src_x, $src_y, $src_w, $src_h) = $location;
+
 		// Source width is larger, use height scale as the base
-		if ($widthScale > $heightScale) {
-			$src_w = round($width * $heightScale);
-
-			// Position horizontally in the middle
-			if ($location === self::CENTER) {
-				$src_x = round(($baseWidth / 2) - (($width / 2) * $heightScale));
-
-			// Position at the far right
-			} else if ($location === self::RIGHT || $location === self::BOTTOM) {
-				$src_x = $baseWidth - $src_w;
-			}
-
-		// Source height is larger, use width scale as the base
 		} else {
-			$src_h = round($height * $widthScale);
+			if ($widthScale > $heightScale) {
+				$src_w = round($width * $heightScale);
 
-			// Position vertically in the middle
-			if ($location === self::CENTER) {
-				$src_y = round(($baseHeight / 2) - (($height / 2) * $widthScale));
+				// Position horizontally in the middle
+				if ($location === self::CENTER) {
+					$src_x = round(($baseWidth / 2) - (($width / 2) * $heightScale));
 
-			// Position at the bottom
-			} else if ($location === self::RIGHT || $location === self::BOTTOM) {
-				$src_y = $baseHeight - $src_h;
+				// Position at the far right
+				} else if ($location === self::RIGHT || $location === self::BOTTOM) {
+					$src_x = $baseWidth - $src_w;
+				}
+
+			// Source height is larger, use width scale as the base
+			} else {
+				$src_h = round($height * $widthScale);
+
+				// Position vertically in the middle
+				if ($location === self::CENTER) {
+					$src_y = round(($baseHeight / 2) - (($height / 2) * $widthScale));
+
+				// Position at the bottom
+				} else if ($location === self::RIGHT || $location === self::BOTTOM) {
+					$src_y = $baseHeight - $src_h;
+				}
 			}
 		}
 
