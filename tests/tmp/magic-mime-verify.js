16941b3bc69b35f1e3cc83d0f873e0092ebaaaf3
Miles Johnson <mileswjohnson@gmail.com>
2013-05-20 19:23:14 -0700

    Fileinfo bug testing [Issue #6]

---
+++ tests/tmp/magic-mime-verify.js
@@ -0,0 +1,7 @@
+/* Let's place a comment at the top of the file to try and trick the mimetype into text/x-c. https://bugs.php.net/bug.php?id=53035 */
+
+var Transit = {
+	method: function() {
+		return 1 + 1;
+	}
+};
