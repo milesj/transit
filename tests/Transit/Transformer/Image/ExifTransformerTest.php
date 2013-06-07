e6bc43b16b908335eb800c8e45efbebd9f5d6885
Miles Johnson <mileswjohnson@gmail.com>
2013-06-07 11:39:50 -0700

    Added portrait and landscape tests

--- tests/Transit/Transformer/Image/ExifTransformerTest.php
+++ tests/Transit/Transformer/Image/ExifTransformerTest.php
@@ -15,7 +15,7 @@ class ExifTransformerTest extends TestCase {
 	/**
 	 * Test that exif orientation is fixed and exif data is stripped.
 	 */
-	public function testTransform() {
+	public function testLandscape() {
 		$exif = array(
 			'make' => '',
 			'model' => '',
@@ -29,7 +29,7 @@ class ExifTransformerTest extends TestCase {
 
 		// 1
 		$or1 = new ExifTransformer();
-		$file = $or1->transform(new File(TEMP_DIR . '/exif/1.jpg'));
+		$file = $or1->transform(new File(TEMP_DIR . '/exif/Landscape_1.jpg'));
 
 		$this->assertEquals(600, $file->width());
 		$this->assertEquals(450, $file->height());
@@ -37,7 +37,7 @@ class ExifTransformerTest extends TestCase {
 
 		// 2
 		$or1 = new ExifTransformer();
-		$file = $or1->transform(new File(TEMP_DIR . '/exif/2.jpg'));
+		$file = $or1->transform(new File(TEMP_DIR . '/exif/Landscape_2.jpg'));
 
 		$this->assertEquals(600, $file->width());
 		$this->assertEquals(450, $file->height());
@@ -45,7 +45,7 @@ class ExifTransformerTest extends TestCase {
 
 		// 3
 		$or1 = new ExifTransformer();
-		$file = $or1->transform(new File(TEMP_DIR . '/exif/3.jpg'));
+		$file = $or1->transform(new File(TEMP_DIR . '/exif/Landscape_3.jpg'));
 
 		$this->assertEquals(600, $file->width());
 		$this->assertEquals(450, $file->height());
@@ -53,7 +53,7 @@ class ExifTransformerTest extends TestCase {
 
 		// 4
 		$or1 = new ExifTransformer();
-		$file = $or1->transform(new File(TEMP_DIR . '/exif/4.jpg'));
+		$file = $or1->transform(new File(TEMP_DIR . '/exif/Landscape_4.jpg'));
 
 		$this->assertEquals(600, $file->width());
 		$this->assertEquals(450, $file->height());
@@ -61,7 +61,7 @@ class ExifTransformerTest extends TestCase {
 
 		// 5
 		$or1 = new ExifTransformer();
-		$file = $or1->transform(new File(TEMP_DIR . '/exif/5.jpg'));
+		$file = $or1->transform(new File(TEMP_DIR . '/exif/Landscape_5.jpg'));
 
 		$this->assertEquals(600, $file->width());
 		$this->assertEquals(450, $file->height());
@@ -69,7 +69,7 @@ class ExifTransformerTest extends TestCase {
 
 		// 6
 		$or1 = new ExifTransformer();
-		$file = $or1->transform(new File(TEMP_DIR . '/exif/6.jpg'));
+		$file = $or1->transform(new File(TEMP_DIR . '/exif/Landscape_6.jpg'));
 
 		$this->assertEquals(600, $file->width());
 		$this->assertEquals(450, $file->height());
@@ -77,7 +77,7 @@ class ExifTransformerTest extends TestCase {
 
 		// 7
 		$or1 = new ExifTransformer();
-		$file = $or1->transform(new File(TEMP_DIR . '/exif/7.jpg'));
+		$file = $or1->transform(new File(TEMP_DIR . '/exif/Landscape_7.jpg'));
 
 		$this->assertEquals(600, $file->width());
 		$this->assertEquals(450, $file->height());
@@ -85,10 +85,90 @@ class ExifTransformerTest extends TestCase {
 
 		// 8
 		$or1 = new ExifTransformer();
-		$file = $or1->transform(new File(TEMP_DIR . '/exif/8.jpg'));
+		$file = $or1->transform(new File(TEMP_DIR . '/exif/Landscape_8.jpg'));
 
 		$this->assertEquals(600, $file->width());
 		$this->assertEquals(450, $file->height());
+		$this->assertEquals($exif, $file->exif());
+	}
+
+	/**
+	 * Test that exif orientation is fixed and exif data is stripped.
+	 */
+	public function testPortrait() {
+		$exif = array(
+			'make' => '',
+			'model' => '',
+			'exposure' => '',
+			'orientation' => '',
+			'fnumber' => '',
+			'date' => '',
+			'iso' => '',
+			'focal' => ''
+		);
+
+		// 1
+		$or1 = new ExifTransformer();
+		$file = $or1->transform(new File(TEMP_DIR . '/exif/Portrait_1.jpg'));
+
+		$this->assertEquals(450, $file->width());
+		$this->assertEquals(600, $file->height());
+		$this->assertEquals($exif, $file->exif());
+
+		// 2
+		$or1 = new ExifTransformer();
+		$file = $or1->transform(new File(TEMP_DIR . '/exif/Portrait_2.jpg'));
+
+		$this->assertEquals(450, $file->width());
+		$this->assertEquals(600, $file->height());
+		$this->assertEquals($exif, $file->exif());
+
+		// 3
+		$or1 = new ExifTransformer();
+		$file = $or1->transform(new File(TEMP_DIR . '/exif/Portrait_3.jpg'));
+
+		$this->assertEquals(450, $file->width());
+		$this->assertEquals(600, $file->height());
+		$this->assertEquals($exif, $file->exif());
+
+		// 4
+		$or1 = new ExifTransformer();
+		$file = $or1->transform(new File(TEMP_DIR . '/exif/Portrait_4.jpg'));
+
+		$this->assertEquals(450, $file->width());
+		$this->assertEquals(600, $file->height());
+		$this->assertEquals($exif, $file->exif());
+
+		// 5
+		$or1 = new ExifTransformer();
+		$file = $or1->transform(new File(TEMP_DIR . '/exif/Portrait_5.jpg'));
+
+		$this->assertEquals(450, $file->width());
+		$this->assertEquals(600, $file->height());
+		$this->assertEquals($exif, $file->exif());
+
+		// 6
+		$or1 = new ExifTransformer();
+		$file = $or1->transform(new File(TEMP_DIR . '/exif/Portrait_6.jpg'));
+
+		$this->assertEquals(450, $file->width());
+		$this->assertEquals(600, $file->height());
+		$this->assertEquals($exif, $file->exif());
+
+		// 7
+		$or1 = new ExifTransformer();
+		$file = $or1->transform(new File(TEMP_DIR . '/exif/Portrait_7.jpg'));
+
+		$this->assertEquals(450, $file->width());
+		$this->assertEquals(600, $file->height());
+		$this->assertEquals($exif, $file->exif());
+
+		// 8
+		$or1 = new ExifTransformer();
+		$file = $or1->transform(new File(TEMP_DIR . '/exif/Portrait_8.jpg'));
+
+		$this->assertEquals(450, $file->width());
+		$this->assertEquals(600, $file->height());
 		$this->assertEquals($exif, $file->exif());
 	}
 
